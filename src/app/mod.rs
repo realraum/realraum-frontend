3d6a96dcb6c118fc6923d0a42ce36056866c54ad
Tanja <git@tanja.pw>
2023-08-06 00:51:14 +0200

    Basic UI

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -6,7 +6,11 @@ pub fn App(cx: Scope) -> impl IntoView {
     let version_info = env!("CARGO_PKG_VERSION");
 
     view! { cx,
-        "Realraum UI" <br/>
-        "Version: " {version_info}
+        <div class="h-fit min-h-screen bg-slate-600 text-white">
+            <div id="topbar" class="sticky top-0 left-0 bg-slate-500 p-1">
+                "Realraum UI v" {env!("CARGO_PKG_VERSION")}
+            </div>
+
+            </div>
     }
 }
